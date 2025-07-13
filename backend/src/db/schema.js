597d4a0427dfb3b65857815e38e0d18f4184f48c
Enaturale <bukolaarowosebi@gmail.com>
2025-07-13 21:56:11 +0100

    serting up the database using neon and drizzle

---
+++ backend/src/db/schema.js
@@ -0,0 +1,13 @@
+import {pgTable, serial, text, timestamp, integer} from "drizzle-orm/pg-core"
+import { use } from "react"
+
+export const favTable = pgTable("favourites", {
+    id: serial("id").primaryKey(),
+    userId: text("user_id").notNull(), 
+    recipeId: integer("recipe_id").notNull(),
+    title: text("title").notNull(),
+    image: text("image"),
+    cookTime: text("cook_time"),
+    servinges: text("servings"),
+    createdAt: timestamp("created_at").defaultNow(),
+})
